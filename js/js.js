3bdc287526469b316ab3bcad821e5bbdcfb1bd6a
manzo1701 <lautaroordenes.epet20@gmail.com>
2021-08-16 10:29:55 -0300

    actualizacionfinal

---
+++ js/js.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const elemntoscarousel = document.querySelectorAll('.carousel');
+    M.carousel.init(elemntoscarousel), {
+        duration: 150
+    };
+
+});
